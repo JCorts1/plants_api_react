adc8afc77206eda8490af572bf324635856f95e1
Jay Cortes <jaycortes@Jays-Air.fritz.box>
2024-04-18 14:18:54 +0200

    Fetch Post option has been added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
-import plantsimg from "./assets/plants.webp"
+import plantsimg from "./assets/plants.webp";
 import PostForm from "./components/PostForm";
 
 const App = () => {
@@ -14,17 +14,43 @@ const App = () => {
     });
   }, []);
   return (
-    <div className="bg-[#363636] w-screen h-screen flex flex-col items-center ">
+    <div className="bg-[#363636] w-screen h-full flex flex-col items-center ">
       <PostForm />
-      <img className="w-3/6 rounded-xl m-20" src={plantsimg} alt="plants image" />
+      <img
+        className="w-3/6 rounded-xl m-20"
+        src={plantsimg}
+        alt="plants image"
+      />
       <div>
         {plants.map((plant) => (
-          <div className="bg-[#626262] w-40 rounded-md m-5">
-            <h1 className="text-xl text-[#fff]">{plant.scientific_name}</h1>
-            <h1 className="text-xl text-[#fff]">{plant.common_name}</h1>
-            <h1>{plant.family}</h1>
-            <div><h1>Toxic to humans:</h1><h1>{plant.humans_toxic ? "yes":"no" }</h1></div>
-            
+          <div className="bg-[#626262] w-80 rounded-md m-5">
+            <div className="flex p-2">
+              <h2 className="text-xl text-white">Scientific Name:</h2> <p className="text-lg ml-2 text-white">{plant.scientific_name}</p>{" "}
+            </div>
+
+            <div className="flex p-2">
+              <h2 className="text-xl text-white">Common Name:</h2> <p className="text-lg text-white ml-2">{plant.common_name}</p>
+            </div>
+
+           <div className="flex p-2">
+            <h2 className="text-xl text-white">Family:</h2> <p className="text-[#D3D6CA] ml-5">{plant.family}</p>
+           </div>
+
+            <div className="flex p-2">
+              <h1 className="text-xl text-white">Toxic to humans:</h1>
+              <p className="text-lg text-white ml-2">{plant.humans_toxic ? "yes" : "no"}</p>
+            </div>
+
+            <div className="flex p-2">
+              <h1 className="text-xl text-white">Toxic to Cats:</h1>
+              <p className="ml-2 text-lg text-white">{plant.cats_toxic ? "yes" : "no "}</p>
+            </div>
+
+            <div className="flex p-2">
+              <h1 className="text-xl text-white">Toxic to Dogs:</h1>
+              <p className="text-lg text-white ml-2">{plants.dogs_toxic ? "yes" : "no"}</p>
+            </div>
+
           </div>
         ))}
       </div>
--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -4,8 +4,8 @@ import axios from "axios";
 
 const PostForm = () => {
   const [plant, setPlant] = useState({
-    streetname: "",
-    sciencename: "",
+    common_name: "",
+    scientific_name: "",
     family: "",
     humansToxic: false,
     catsToxic: false,
@@ -14,36 +14,104 @@ const PostForm = () => {
 
   function handleSubmit(event) {
     event.preventDefault();
-    axios.post("http://127.0.0.1:3000/foods", { plants: plant });
+    console.log(plant)
+    axios.post("http://127.0.0.1:3000/plants", { plants: plant });
   }
 
-  return (
-    <div className="bg-[#47BE35] rounded-3xl w-2/4 flex items-center justify-center">
-      <form>
-        <div className="m-3">
-          <label className="text-white text-xl mr-4"  htmlFor="streetName">Common Name:</label>
-          <input onChange={(event)=> {
-            setPlant({...plant, streetname:event.target.value})
-          }} className="rounded-lg" id="streetName" type="text" />
-        </div>
-        <div className="m-3">
-          <label className="text-white text-xl mr-4" htmlFor="scienceName">Scientific Name:</label>
-          <input onChange={(event)=> {
-            setPlant({...plant, sciencename:event.target.value})
-          }} className="rounded-lg" id="scienceName" type="text" />
-        </div>
-        <div className="m-3">
-          <label className="text-white text-xl mr-24" htmlFor="family">Family:</label>
-          <input onChange={(event)=> {
-            setPlant({...plant, family:event.target.value})
-          }} className="rounded-lg" id="family" type="text" />
-        </div> 
-      </form>
-      <div>
-            <button type="submit" className="bg-[#828282] w-20 h-8 rounded-xl text-white">Insert</button>
-        </div>
+  const handleCheckboxChange = (event) => {
+    setPlant({ ...plant, humansToxic: event.target.checked ? true : false });
+  };
 
-    </div>
+  const handleCatsToxicChange = (event) => {
+    setPlant({ ...plant, catsToxic: event.target.checked ? true : false });
+  };
+
+  const handleDogsToxicChange = (event) => {
+    setPlant({ ...plant, dogsToxic: event.target.checked ? true : false });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+
+      <div className="m-3">
+        <label className="text-white text-xl mr-4" htmlFor="streetName">
+          Common Name:
+        </label>
+        <input
+          onChange={(event) => {
+            setPlant({ ...plant, common_name: event.target.value });
+          }}
+          className="rounded-lg"
+          id="streetName"
+          type="text"
+        />
+      </div>
+
+      <div className="m-3">
+        <label className="text-white text-xl mr-4" htmlFor="scienceName">
+          Scientific Name:
+        </label>
+        <input
+          onChange={(event) => {
+            setPlant({ ...plant, scientific_name: event.target.value });
+          }}
+          className="rounded-lg"
+          id="scienceName"
+          type="text"
+        />
+      </div>
+
+      <div className="m-3">
+        <label className="text-white text-xl mr-24" htmlFor="family">
+          Family:
+        </label>
+        <input
+          onChange={(event) => {
+            setPlant({ ...plant, family: event.target.value });
+          }}
+          className="rounded-lg"
+          id="family"
+          type="text"
+        />
+      </div>
+
+      <div className="mb-2">
+        <label
+          className="text-white text-xl"
+          htmlFor="humanstoxic">
+          Toxic to humans?
+        </label>
+        <input
+        checked={plant.humansToxic}
+        onChange={handleCheckboxChange}
+        className="ml-4" id="humanstoxic" type="checkbox" />
+      </div>
+
+      <div className="mb-2">
+        <label className="text-white text-xl" htmlFor="catstoxic">
+          Toxic to cats?
+        </label>
+        <input
+        checked = {plant.catsToxic}
+        onChange={handleCatsToxicChange}
+        className="ml-12" id="catstoxic" type="checkbox" />
+      </div>
+      <div className="mb-2">
+        <label className="text-white text-xl" htmlFor="dogstoxic">
+          Toxic to dogs??
+        </label>
+        <input 
+        checked= {plant.dogsToxic}
+        onChange= {handleDogsToxicChange}
+        className="ml-8" id="dogstoxic" type="checkbox" />
+      </div>
+      <button
+        type="submit"
+        className="bg-[#828282] w-20 h-8 rounded-xl text-white mt-4 mb-4"
+      >
+        Insert
+      </button>
+    </form>
   );
 };
 
